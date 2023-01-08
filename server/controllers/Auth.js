07aa44d4022a83a392875a4e3438c6e8830d679b
marques <marqueschristmann8@gmail.com>
2023-01-08 20:00:14 -0300

    updating jwt token

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -14,10 +14,11 @@ export const refreshToken = async(req, res) => {
         if(!user[0]) return res.sendStatus(403);
         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
             if(err) return res.sendStatus(403);
-            const userId = user[0].id;
-            const name = user[0].name;
-            const email = user[0].email;
-            const accessToken = jwt.sign({userId, name, email}, process.env.ACCESS_TOKEN_SECRET,{
+        const uuid = user.uuid;
+        const name = user.name;
+        const email = user.email;
+        const role = user.role;
+            const accessToken = jwt.sign({uuid, name, email, role}, process.env.ACCESS_TOKEN_SECRET,{
                 expiresIn: '15s'
             });
             res.json({ accessToken });
